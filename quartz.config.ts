3d9170381cd867ad452c4f25e95a79aa79476c2a
Agustín Bravo Pérez <anbravoperez@gmail.com>
2025-01-04 03:24:29 -0300

    pipeline: hacer `git fetch --all` después de clonar `agustinbravop/utn-vault`

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "es-ES",
-    baseUrl: "agustinbravop.github.io",
+    baseUrl: "agustinbravop.github.io/utn-vault-website",
     ignorePatterns: [
       "private",
       "templates",
