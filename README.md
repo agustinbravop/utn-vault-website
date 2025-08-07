81ebb5c7d8a0530338f9dbf88f62922123a6e15a
agustinbravop <anbravoperez@gmail.com>
2025-08-07 19:01:57 -0300

    agregar google analytics

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # utn-vault-website
 
 Generador del sitio web para mi Obsidian Vault con mis apuntes de Ingeniería en Sistemas de Información cursada en la UTN FRRe.
+
+Levantar el web server local con:
+
+```bash
+npx quartz build --serve
+```
--- quartz.config.ts
+++ quartz.config.ts
@@ -13,7 +13,8 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: "google",
+      tagId: "G-VG7TMJ2LDX",
     },
     locale: "es-ES",
     baseUrl: "agustinbravop.github.io/utn-vault-website",
